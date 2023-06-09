c3c1d2cde7d104c92f4fe6f26062fff913729e17
Wade Wilson <sugumarsg17@@gmail.com>
2023-06-09 13:16:42 +0530

    Added external sprites for svg

--- dist/index.740070ea.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll(".collapsible");e.forEach(e=>e.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));const c=document.querySelectorAll(".domain-block__prices li");c.forEach(e=>e.addEventListener("click",function(){c.forEach(e=>e.classList.remove("badge-primary")),this.classList.toggle("badge-primary")}));
-//# sourceMappingURL=index.740070ea.js.map
